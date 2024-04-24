1778c2d47c75dde74631881f2c7d7701753198d9
glihm <dev@glihm.net>
2024-04-24 13:50:46 -0600

    feat: add Abigen to generate bindings without macros

---
+++ examples/abigen_generate.rs
@@ -0,0 +1,20 @@
+use cainome::rs::Abigen;
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() {
+    let mut aliases = HashMap::new();
+    aliases.insert(String::from("my::type::Event"), String::from("MyTypeEvent"));
+
+    let abigen = Abigen::new(
+        "MyContract",
+        "./contracts/target/dev/contracts_simple_get_set.contract_class.json",
+    )
+    .with_types_aliases(aliases);
+
+    abigen
+        .generate()
+        .expect("Fail to generate bindings")
+        .write_to_file("/tmp/abigen.rs")
+        .unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,6 @@ pub mod parser {
 
 #[cfg(feature = "abigen-rs")]
 pub mod rs {
-    pub use cainome_rs_macro::*;
     pub use cainome_rs::*;
+    pub use cainome_rs_macro::*;
 }
