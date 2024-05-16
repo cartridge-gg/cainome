0abcc370a8dbfdf3ddbd868916ebec60bb6791ab
glihm <dev@glihm.net>
2024-05-16 17:26:59 +0200

    docs: remove empty docs

--- crates/rs/src/expand/utils.rs
+++ crates/rs/src/expand/utils.rs
@@ -3,22 +3,18 @@ use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use syn::{Ident, LitInt, LitStr, Type};
 
-///
 pub fn str_to_ident(str_in: &str) -> Ident {
     Ident::new(str_in, proc_macro2::Span::call_site())
 }
 
-///
 pub fn str_to_type(str_in: &str) -> Type {
     syn::parse_str(str_in).unwrap_or_else(|_| panic!("Can't convert {} to syn::Type", str_in))
 }
 
-///
 pub fn str_to_litstr(str_in: &str) -> LitStr {
     LitStr::new(str_in, proc_macro2::Span::call_site())
 }
 
-///
 pub fn str_to_litint(str_in: &str) -> LitInt {
     LitInt::new(str_in, proc_macro2::Span::call_site())
 }
