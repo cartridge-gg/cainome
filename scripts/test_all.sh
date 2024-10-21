83d7d57e6c69d60b2780405afdbe3ae504a46dc6
glihm <dev@glihm.net>
2024-10-20 22:01:13 -0600

    fix: add some example to the test suite waiting proper tests

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -1,1 +1,6 @@
 cargo test --workspace --all-features
+
+# Somes examples are currently containing some generated
+# code to test the serde implementation.
+# TODO: this should be moved to the test suite.
+cargo run --example structs --all-features
