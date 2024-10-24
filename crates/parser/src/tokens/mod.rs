e248d513510bae035827eb262781976131ebe13d
Valentin Dosimont <valentin@pupucecorp.com>
2024-10-24 15:27:58 +0200

    feat: enable nested type hydration

--- crates/parser/src/tokens/mod.rs
+++ crates/parser/src/tokens/mod.rs
@@ -10,6 +10,8 @@ mod function;
 mod genericity;
 mod tuple;
 
+use std::collections::HashMap;
+
 pub use array::Array;
 pub use basic::CoreBasic;
 pub use composite::{Composite, CompositeInner, CompositeInnerKind, CompositeType};
@@ -119,6 +121,83 @@ impl Token {
             Token::Composite(t) => t.apply_alias(type_path, alias),
             Token::Function(t) => t.apply_alias(type_path, alias),
             _ => (),
+        }
+    }
+
+    /// Recursively hydrates nested tokens
+    ///
+    /// Once abi is parsed, a flat list of tokens defined in cairo code is generated from parsed
+    /// json abi string.
+    /// Then token list are filtered to only keep single copy of each token.
+    /// Some tokens can have nested tokens that may not have inners defined inside thus leading to
+    /// confusion while using tokens. i.e Enums does not have inner variants defined.
+    ///
+    /// # Arguments
+    ///
+    /// * `token` - The token to hydrate.
+    /// * `filtered` - A map of type path to token that have already been hydrated.
+    ///
+    pub fn hydrate(token: Self, filtered: &HashMap<String, Token>) -> Self {
+        match token {
+            Token::CoreBasic(_) | Token::GenericArg(_) => token,
+            Token::Array(arr) => Token::Array(Array {
+                inner: Box::new(Self::hydrate(*arr.inner, filtered)),
+                type_path: arr.type_path,
+                is_legacy: arr.is_legacy,
+            }),
+            Token::Tuple(tup) => Token::Tuple(Tuple {
+                inners: tup
+                    .inners
+                    .into_iter()
+                    .map(|inner| Self::hydrate(inner, filtered))
+                    .collect(),
+                type_path: tup.type_path,
+            }),
+            Token::Composite(comp) => {
+                if comp.r#type == CompositeType::Unknown && !comp.is_builtin() {
+                    if let Some(hydrated) = filtered.get(&comp.type_path) {
+                        return hydrated.clone();
+                    } else {
+                        panic!("Composite {} not found in filtered tokens", comp.type_path);
+                    }
+                }
+                Token::Composite(Composite {
+                    type_path: comp.type_path,
+                    inners: comp
+                        .inners
+                        .into_iter()
+                        .map(|i| CompositeInner {
+                            index: i.index,
+                            name: i.name,
+                            kind: i.kind,
+                            token: Self::hydrate(i.token, filtered),
+                        })
+                        .collect(),
+                    generic_args: comp.generic_args,
+                    r#type: comp.r#type,
+                    is_event: comp.is_event,
+                    alias: comp.alias,
+                })
+            }
+            Token::Function(func) => Token::Function(Function {
+                name: func.name,
+                inputs: func
+                    .inputs
+                    .into_iter()
+                    .map(|(name, token)| (name, Self::hydrate(token, filtered)))
+                    .collect(),
+                outputs: func
+                    .outputs
+                    .into_iter()
+                    .map(|token| Self::hydrate(token, filtered))
+                    .collect(),
+                named_outputs: func
+                    .named_outputs
+                    .into_iter()
+                    .map(|(name, token)| (name, Self::hydrate(token, filtered)))
+                    .collect(),
+                state_mutability: func.state_mutability,
+            }),
         }
     }
 }
