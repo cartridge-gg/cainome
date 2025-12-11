84ae06c8547e866ea3375f2ad71689da4c4f3bec
baitcode <batiyiv@gmail.com>
2025-12-11 18:58:05 +0100

    unused code

--- crates/rs/src/expand/types.rs
+++ crates/rs/src/expand/types.rs
@@ -204,18 +204,3 @@ fn basic_types_to_rust(type_name: &str) -> String {
         _ => type_name.to_string(),
     }
 }
-
-// fn builtin_composite_to_rust(type_name: &str) -> (String, bool) {
-//     let ccsp = utils::cainome_cairo_serde_path();
-//     let snrs_types = utils::starknet_rs_types_path();
-
-//     match type_name {
-//         "EthAddress" => (format!("{ccsp}::EthAddress"), true),
-//         "ByteArray" => (format!("{ccsp}::ByteArray"), true),
-//         "NonZero" => (format!("{ccsp}::NonZero"), true),
-//         "U256" => (format!("{ccsp}::U256"), true),
-//         // <https://github.com/starkware-libs/cairo/blob/35b299291fd7819f75409fb303ece7d30e4adb19/corelib/src/internal/bounded_int.cairo#L5>
-//         "BoundedInt" => (format!("{snrs_types}::Felt"), true),
-//         _ => (type_name.to_string(), false),
-//     }
-// }
